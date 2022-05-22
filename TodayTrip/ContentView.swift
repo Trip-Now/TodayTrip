4239dc69b07ea111425e65e175812b3fa39eedab
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2022-05-22 20:53:06 +0900

    Initial Commit

---
+++ TodayTrip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodayTrip
+//
+//  Created by Junho Son on 2022/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("김기찬 Hello")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodayTrip/TodayTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayTripApp.swift
+//  TodayTrip
+//
+//  Created by Junho Son on 2022/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TodayTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
